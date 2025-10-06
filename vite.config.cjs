1dfb905a4c1c967b37c521a7b89922cf541c3f1d
Kukkapalli Abhiram <78559926+AbhiramKukkapalli@users.noreply.github.com>
2025-10-06 02:10:57 -0400

    Update and rename vite.config.mjs to vite.config.cjs

---
+++ vite.config.cjs
@@ -0,0 +1,9 @@
+// goat-farm-app/vite.config.cjs
+
+const { defineConfig } = require('vite')
+const react = require('@vitejs/plugin-react')
+
+// https://vitejs.dev/config/
+module.exports = defineConfig({
+  plugins: [react()],
+})
